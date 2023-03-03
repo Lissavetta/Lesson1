e3c682c46918112b9a8f3d8d5e4c4fc21d7893ce
ElizavetaTropina <elizzzabet0603@gmail.com>
2023-03-03 14:55:44 +0300

    cat bread

--- typelayout/src/main/java/ru/mirea/tropina/typelayout/MainActivity.java
+++ typelayout/src/main/java/ru/mirea/tropina/typelayout/MainActivity.java
@@ -3,12 +3,28 @@ package ru.mirea.tropina.typelayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvMirea;
+    private Button btnChangeMireaText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvMirea = findViewById(R.id.textViewMirea);
+        btnChangeMireaText = findViewById(R.id.buttonChangeMireaText);
+
+
+
+
+
+
+
+
     }
 }
